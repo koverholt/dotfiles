3773efa4bc1b05037dfbbfd5c4de8fb718bb7d4a
Kristopher Overholt <koverholt@gmail.com>
2014-09-27 18:00:04 -0400

    Add OS X defaults

---
+++ osx/set_defaults.sh
@@ -0,0 +1,12 @@
+# Sets OS X defaults.
+#
+
+# Always open everything in Finder's list view
+defaults write com.apple.Finder FXPreferredViewStyle Nlsv
+
+# Show the ~/Library folder
+chflags nohidden ~/Library
+
+# Set a fast key repeat
+defaults write NSGlobalDomain KeyRepeat -int 0
+
